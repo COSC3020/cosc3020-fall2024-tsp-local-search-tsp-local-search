14494febb5a4e3f24024c7a54e83e5f9108094e7
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-11-25 12:32:34 -0700

    Update code.js

--- code.js
+++ code.js
@@ -44,25 +44,3 @@ function swap(arr, r, l) {
     l--;
   }
 }
-dm = [[]];
-console.log(tsp_ls(dm) == 0);
-
-dm = [[0]];
-console.log(tsp_ls(dm) == 0);
-
-dm = [[0,0,0],
-      [0,0,0],
-      [0,0,0]];
-console.log(tsp_ls(dm) == 0);
-
-dm = [[0,1,2],
-      [1,0,2],
-      [2,2,0]];
-console.log(tsp_ls(dm) >= 3);
-
-dm = [[0,3,4,2,7],
-      [3,0,4,6,3],
-      [4,4,0,5,8],
-      [2,6,5,0,6],
-      [7,3,8,6,0]];
-console.log(tsp_ls(dm) >= 13);
