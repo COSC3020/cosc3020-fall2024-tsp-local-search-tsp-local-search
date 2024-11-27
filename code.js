30ec776543e40edfc0c628f8650dd38309ec979c
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-11-27 14:05:05 -0700

    Update code.js

--- code.js
+++ code.js
@@ -4,37 +4,32 @@ function tsp_ls(distance_matrix) {
   let minRoute = Array.from({ length: n }, (v, i) => i).sort((a, b) => 0.5 - Math.random());
   let minCost = calcCost(minRoute, distance_matrix);
   let improving = true;
-  
   while(improving) {
     let routePath = [...minRoute]; // Get a deep copy of min route
     let r = Math.floor(Math.random() * (n - 1)) + 1; // Random Number in the range 1 to n - 1
     let l = Math.floor(Math.random() * (n - r - 1)) + (r + 1); // Random number in the range r + 1 to n
-    
-    // If the swap was already done don`t undo it
-    if (memo.has(`${r}, ${l}`)) continue;
-    improving = false;
-    memo.add(`${r}, ${l}`);
     swap(routePath, r, l);
-
+    // If route was already computed don't recompute it
+    let routeKey = routePath.join(",");
+    if (memo.has(routeKey)) continue;
+    memo.add(routeKey);
     let newCost = calcCost(routePath, distance_matrix);
-
     if (newCost < minCost) {
       minCost = newCost;
       minRoute = routePath;
       improving = true;
     }
+    else improving = false; // no improvement
   }
-  
   return minCost;
 }
- function calcCost(route, distance_matrix){
+function calcCost(route, distance_matrix){
     let newCost = 0;
     for (let i = 0; i < distance_matrix.length - 1; i++) {
       newCost += distance_matrix[route[i]][route[i + 1]];
     }
     return newCost;
- }
-
+}
 function swap(arr, r, l) {
   while (r < l) {
     let temp = arr[r];
